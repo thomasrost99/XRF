9fb29952e6af8700c2870fb0ee938b2a2a522a08
Thomas Rost <Thomas.Rost.99@gmail.com>
2021-03-22 18:16:01 -0500

    Added basic csv file browsing functionality

--- app.py
+++ app.py
@@ -1,30 +1,53 @@
 import signal
 import sys
+import csv
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 
+QWidget
+{
+    color: #b1b1b1;
+    background-color: #323232;
+    selection-background-color:#323232;
+    selection-color: black;
+    background-clip: border;
+    border-image: none;
+    border: 0px transparent black;
+    outline: 0;
+}
+
 class MainWindow(QMainWindow):
-    def __init__(self, parent=None):
-        super(MainWindow, self).__init__(parent)
-        self.setWindowTitle("MaXelerate")
+   def __init__(self, parent=None):
+      super(MainWindow, self).__init__(parent)
+      self.setWindowTitle("MaXelerate")
 
-        btn = QPushButton("Open File")
-        layout = QVBoxLayout()
-        layout.addWidget(btn)
-        widget = QWidget()
-        widget.setLayout(layout)
-        self.setCentralWidget(widget)
+      btn = QPushButton("Open File")
+      layout = QHBoxLayout()
+      layout.addWidget(btn)
+      widget = QWidget()
+      widget.setLayout(layout)
+      self.setCentralWidget(widget)
 
-        btn.clicked.connect(self.open) # connect clicked to self.open()
-        self.show()
+      btn.clicked.connect(self.open) # connect clicked to self.open()
+      self.setStyleSheet("border: 2px solid black; background:grey")
+      self.show()
 
-    def open(self):
-        path = QFileDialog.getOpenFileName(self, 'Open a file', '', 'All Files (*.*)')
-        if path != ('', ''): print("File path : "+ path[0])
+   def open(self):
+      dialog = QFileDialog
+      res = dialog.getOpenFileName(self, 'Open file', 'c:\\Users\\thoma\\Downloads',"Csv files (*.csv)")
+      file = open(res[0] ,'r')
+      reader = csv.reader(file)
+      line_count = 0
+      for row in reader:
+         qDebug(row[5])
+         line_count += 1
+      file.close()
+      qDebug(res[0])
+
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    signal.signal(signal.SIGINT, signal.SIG_DFL)
-    sys.exit(app.exec_())
+   app = QApplication(sys.argv)
+   window = MainWindow()
+   signal.signal(signal.SIGINT, signal.SIG_DFL)
+   app.exec()
