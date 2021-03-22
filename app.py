5cb1fa7a7bd1edb0df20575d5d9026728239ecec
Hayden Roper <55068483+HaydenRoper6@users.noreply.github.com>
2021-03-22 15:35:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,31 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import pyqtSlot
+
 
 def window():
    app = QApplication(sys.argv)
    widget = QWidget()
 
+   layout = QVBoxLayout()
    textLabel = QLabel(widget)
-   textLabel.setText("Hello World!")
-   textLabel.move(110,85)
+   textLabel.setText("<h1>Jack is a retard whore!</h1>")
+   #textLabel.move(10,10)
+   layout.addWidget(textLabel)
 
-   widget.setGeometry(50,50,320,200)
+   layout.addWidget(QPushButton("Upload File"))
+   layout.addWidget(QPushButton("Choose Element"))
+   layout.addWidget(QPushButton("Make Graph"))
+
+   widget.setGeometry(300,300,300,300)
    widget.setWindowTitle("PyQt5 Example")
+   widget.setLayout(layout)
    widget.show()
+
    sys.exit(app.exec_())
+
+
 
 if __name__ == '__main__':
     window()
