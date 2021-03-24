bf6ba095d88cff6c29da6f001a5f1c0564b96b92
Hayden Roper <55068483+HaydenRoper6@users.noreply.github.com>
2021-03-24 17:55:22 -0500

    Update testWizard.py

--- testWizard.py
+++ testWizard.py
@@ -42,17 +42,51 @@ class MacWizard(QtWidgets.QWizard):
         self.setPage(inputPageID, self.inputPage)
         self.setPage(elementPageID, self.elementPage)
 
-
-
         #link all buttons to proper functions
         self.button(QWizard.NextButton).clicked.connect(self.nextClicked)
         self.button(QWizard.FinishButton).clicked.connect(self.finishClicked)
         self.button(QWizard.CancelButton).clicked.connect(self.cancelClicked)
         self.button(QWizard.BackButton).clicked.connect(self.backClicked)
 
+    def lockButton(self, name):
+        print('LOCKED' + name)
+        if(name=='back'):
+            self.button(QWizard.BackButton).setEnabled(False)
+        elif(name=='next'):
+            self.button(QWizard.NextButton).setEnabled(False)
+        elif(name=='cancel'):
+            self.button(QWizard.CancelButton).setEnabled(False)
+        elif(name=='finish'):
+            self.button(QWizard.FinishButton).setEnabled(False)
+        else:
+            return False
+        return True
+
+    def unlockButton(self, name):
+        print('UNLOCKED' + name)
+        if(name=='back'):
+            self.button(QWizard.BackButton).setEnabled(True)
+        elif(name=='next'):
+            self.button(QWizard.NextButton).setEnabled(True)
+        elif(name=='cancel'):
+            self.button(QWizard.CancelButton).setEnabled(True)
+        elif(name=='finish'):
+            self.button(QWizard.FinishButton).setEnabled(True)
+        else:
+            return False
+        return True
+
+
     def nextClicked(self):
-        print(QWizard.currentPage(self))
+        #print(QWizard.currentPage(self))
         print(QWizard.currentId(self))
+        pageID = QWizard.currentId(self)
+        if(pageID == inputPageID):
+            self.lockButton('back')
+            print("Check that input is right then advance")
+
+        elif(pageID == elementPageID):
+            print("Verify elements selected are valid then advance")
         #self.button(QWizard.BackButton).setEnabled(False)
         #print(self.elementPage.selectedElements)
 
